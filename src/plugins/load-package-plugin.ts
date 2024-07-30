80f48f3a8e86249c82a56bb119caa91a73ee3b98
mstharindu <supuntharindu21@gmail.com>
2024-07-31 00:45:10 +0530

    Fetching css packages

--- src/plugins/load-package-plugin.ts
+++ src/plugins/load-package-plugin.ts
@@ -5,20 +5,31 @@ const packageCache = localforage.createInstance({
   name: 'packageCache',
 });
 
+const fetchPackage = async (
+  path: string
+): Promise<{ packageContent: string; responseUrl: string }> => {
+  const response = await fetch(path);
+
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+
+  const packageContent = await response.text();
+
+  return { packageContent, responseUrl: response.url };
+};
+
 export const loadPackagePlugin = (input: string) => {
   return {
     name: 'load-package-plugin',
     setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: input,
+        };
+      });
       build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
-        console.log('onLoad', args);
-
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: input,
-          };
-        }
-
         const packageContent = await packageCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
@@ -26,19 +37,37 @@ export const loadPackagePlugin = (input: string) => {
         if (packageContent) {
           return packageContent;
         }
+      });
 
-        const response = await fetch(args.path);
+      build.onLoad({ filter: /.css$/ }, async (args: esbuild.OnLoadArgs) => {
+        const { packageContent, responseUrl } = await fetchPackage(args.path);
 
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
-        }
-
-        const text = await response.text();
+        const escapedContent = packageContent
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
 
         const packagePayload: esbuild.OnLoadResult = {
           loader: 'jsx',
-          contents: text,
-          resolveDir: `${new URL('./', response.url).pathname}`,
+          contents: `const styleTag = document.createElement('style');
+        styleTag.innerText = '${escapedContent}';
+        document.head.appendChild(styleTag);`,
+          resolveDir: `${new URL('./', responseUrl).pathname}`,
+        };
+
+        await packageCache.setItem(args.path, packagePayload);
+
+        //const json = await response.json();
+        return packagePayload;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
+        const { packageContent, responseUrl } = await fetchPackage(args.path);
+
+        const packagePayload: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: packageContent,
+          resolveDir: `${new URL('./', responseUrl).pathname}`,
         };
 
         await packageCache.setItem(args.path, packagePayload);
