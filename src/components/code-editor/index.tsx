896b88e3aaabf24caf8ae64f92820c879f2de93f
mstharindu <supuntharindu21@gmail.com>
2024-08-02 12:50:04 +0530

    Integrating monaco code editor

--- src/components/code-editor/index.tsx
+++ src/components/code-editor/index.tsx
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { build } from '../../bundler';
+import { Editor } from '@monaco-editor/react';
+import { editor } from 'monaco-editor';
 
 interface CodeEditorProps {
   setCode: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export const CodeEditor: React.FC<CodeEditorProps> = ({ setCode }) => {
-  const [input, setInput] = useState('');
+  //const [input, setInput] = useState('');
+
+  const editorRef = useRef<editor.ICodeEditor | null>(null);
+
+  const handleEditorDidMount = (editor: editor.ICodeEditor) => {
+    editorRef.current = editor;
+  };
+
+  const getValue = () => {
+    if (editorRef.current) return editorRef.current.getValue();
+
+    return '';
+  };
 
   const onClickSubmit = async () => {
+    const input = getValue();
     const code = await build(input);
     setCode(code);
   };
@@ -22,10 +37,12 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({ setCode }) => {
         width: '50%',
       }}
     >
-      <textarea
-        style={{ height: '100vh' }}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
+      <Editor
+        height="100vh"
+        defaultLanguage="javascript"
+        defaultValue="//Write your code here"
+        onMount={handleEditorDidMount}
+      />
       <button
         onClick={onClickSubmit}
         style={{ position: 'absolute', right: 0, bottom: '0.5rem' }}
